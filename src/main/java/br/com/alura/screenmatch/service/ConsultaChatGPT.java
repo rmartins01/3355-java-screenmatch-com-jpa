587d397eb66da0c0119de5f8a85ab8c4cabb633b
Robson Martins <robson.java@gmail.com>
2024-09-16 14:56:19 -0300

    removed key of Open IA

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -6,7 +6,7 @@ import com.theokanning.openai.service.OpenAiService;
 public class ConsultaChatGPT {
 	
 	public static String obterTraducao(String texto) {
-		OpenAiService service = new OpenAiService("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");
+		OpenAiService service = new OpenAiService("Substituir");
 
 		CompletionRequest requisicao = CompletionRequest.builder().model("gpt-3.5-turbo-instruct")
 				.prompt("traduza para o português o texto: " + texto).maxTokens(10).temperature(0.7).build();
